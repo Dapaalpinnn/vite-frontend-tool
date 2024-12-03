a008e8aff712be84f528caa2e538d6ae63283bfd
Dafa Alvin <dafaalvin10@gmail.com>
2024-12-03 07:49:54 +0700

    json

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import { hello } from "./hello.js";
 import { say } from "./say.ts";
 import { v4 } from "uuid";
+import * as data from "./test.json"
 
 hello("Vite");
 say("Hi");
 console.log(v4());
+console.log(data.name);
+console.log(data.description);
