d385be24c6521520f65f08c2e7710f72e976e2df
Dafa Alvin <dafaalvin10@gmail.com>
2024-12-03 08:14:40 +0700

    static assets

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import { hello } from "./hello.js";
 import { say } from "./say.ts";
 import { v4 } from "uuid";
 import * as data from "./test.json"
+import hi from "./hi.txt?raw"
 
 hello("Vite");
 say("Hi");
 console.log(v4());
 console.log(data.name);
 console.log(data.description);
+console.log(hi);
